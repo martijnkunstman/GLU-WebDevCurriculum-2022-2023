af10e1d4a678c23abe95df43060e46fc2897cfd6
martijnkunstman <martijn@kunstman.nl>
2022-04-23 00:50:53 +0200

    planning

--- planning.js
+++ planning.js
@@ -22,12 +22,35 @@ function createPlanningMap() {
     for (let i = 0; i < planningData.schoolYearIds.length; i++) {
         let schoolYear = planningConfig.schoolYears.find(schoolYear => schoolYear.id === planningData.schoolYearIds[i]);
         app.innerHTML += '<div id="subtitle">Leerjaar ' + (i + 1) + ' - ' + schoolYear.years.join("-") + '</div>';
-        let start = schoolYear.quarters[0][0]-1;
-        let end = schoolYear.quarters[schoolYear.quarters.length - 1][1]+52;
+        let start = schoolYear.quarters[0][0] - 1;
+        let end = schoolYear.quarters[schoolYear.quarters.length - 1][1] + 52;
         for (let j = start; j < end; j++) {
-            app.innerHTML += '<div id="scedule-item">|' + (j%52 + 1) + '|</div>';
+            let quarter = findQuarterByWeek((j % 52 + 1), schoolYear.quarters, i);
+            let holiday = findHolidays((j % 52 + 1), schoolYear.holidays);
+            app.innerHTML += '<div id="scedule-item">|' + (j % 52 + 1) + '-' + quarter + '-'+holiday+'|</div>';
         }
     }
+}
+
+function findHolidays(week, holidays) {
+    console.log(holidays);
+    for (let i = 0; i < holidays.length; i++) {
+        for (let j = 0; j < holidays[i].weeks.length; j++) {
+            if (week === holidays[i].weeks[j]) {
+                return holidays[i].name;
+            }
+        }        
+    }
+    return "";
+}
+
+function findQuarterByWeek(week, quarters, year) {
+    for (let i = 0; i < quarters.length; i++) {
+        if (week >= quarters[i][0] && week <= quarters[i][1]) {
+            return (i + 1 + year * 4);
+        }
+    }
+    return (2 + year * 4);
 }
 
 
